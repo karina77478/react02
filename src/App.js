c3cf68e6f733bb1382770b5ed9b92cabc06460c0
karina77478 <bordakari9@gmail.com>
2023-10-18 18:59:48 -0500

    v5. diseno app, appform

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import AppForm from './componets/AppForm';
+import logo from './logo.svg';
+//import './App.css';
+import C01componente from './pagina/C01componente';
+
+function App() {
+  return (
+    <div style={{width:"350px", background:"greenyellow", padding:"10px"}}>
+      <AppForm />
+      <p>No. 1 Karina Luz        x   A</p>
+      <p>No. 2 Ana Maria       x   A</p>
+    </div>
+  );
+}
+
+export default App;
