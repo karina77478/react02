4cb766ae0d36ca86191401d44c3cb25a588a80a1
karina77478 <bordakari9@gmail.com>
2023-10-18 19:12:55 -0500

    V5.02  diseño app y appform

---
+++ src/componets/C04NuevoCom.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const C04NuevoCom = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default C04NuevoCom
---
+++ src/componets/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAVqOuH7j-XB96CPF7Lb0lnpVxyb7yx2oA",
+  authDomain: "appreact-1f782.firebaseapp.com",
+  projectId: "appreact-1f782",
+  storageBucket: "appreact-1f782.appspot.com",
+  messagingSenderId: "844955147948",
+  appId: "1:844955147948:web:6998a837c194cf7bff8909"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
