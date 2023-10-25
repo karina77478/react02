8a6496ce43683bd062f499a8ff664b2e2940c703
karina77478 <bordakari9@gmail.com>
2023-10-25 16:05:20 -0500

    V6 se confijuro firebase

--- src/componets/AppForm.js
+++ src/componets/AppForm.js
@@ -1,13 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { db } from './firebase';
+import { addDoc, collection } from 'firebase/firestore';
 
 const AppForm = () => {
+    ////GUARDAR / ACTUALIZAR ////
+    const camposRegistro = {nombre:"",edad:"",genero:""}
+    const [objeto, setObjeto] = useState(camposRegistro);
+
+  const handleSubmit = (e) =>{ 
+    e.preventDefault();
+    try {
+      if(db){
+        addDoc(collection(db,'persona'), objeto);
+        console.log("Se guardo con exito");
+      }
+
+  } catch (error) {
+    console.error();
+  }
+
+  }
   return (
     <div style={{background:"orange", padding:"10px", textAlign:"center"}}>
+    <form onSubmit={handleSubmit}>
       <h1>AppForm.js</h1>
-      <p>Nombre</p>
-      <p>Edad</p>
-      <p>Genero</p>
-      <p>Guardar</p>
+      <input type='texto' placeholder='Nombre.....'/>
+      <input type='texto' placeholder='Nombre.....'/>
+      <select>
+      <option value="">Seleccione</option>
+      <option value="M">Masculino</option>
+      <option value="F">Femenino</option>
+      </select>
+      <button>Guardar/Actualizar</button>
+    </form>
     </div>
   )
 }
